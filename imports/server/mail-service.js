92c66afd9cd895d8368bbb03d1edddb61f301eec
Vitali Hatalski <v.hatalski@gmail.com>
2016-12-16 10:53:01 +0300

    common env var

--- imports/server/mail-service.js
+++ imports/server/mail-service.js
@@ -1,7 +1,7 @@
 import { Email } from 'meteor/email';
 import { check } from 'meteor/check';
 
-process.env.MAIL_URL = "smtp://postmaster%40sandbox722d1efe68a74cc0bd4c02857b91d16a.mailgun.org:f0ac033a1f6adf295725845148d22cd6@smtp.mailgun.org:587";
+//process.env.MAIL_URL = "smtp://postmaster%40sandbox722d1efe68a74cc0bd4c02857b91d16a.mailgun.org:f0ac033a1f6adf295725845148d22cd6@smtp.mailgun.org:587";
 
 Meteor.methods({
   sendEmail:function(mail) {
