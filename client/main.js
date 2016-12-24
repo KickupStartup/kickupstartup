9eed054ea6142925b2d23a047df4e87216cb5dd0
Vitali Hatalski <v.hatalski@gmail.com>
2016-12-24 22:44:13 +0300

    setting up ui libs and images

--- client/main.js
+++ client/main.js
@@ -6,3 +6,9 @@ import '../imports/startup/client';
 // Meteor.startup(() => {
 //   render(<App />, document.getElementById('render-target'));
 // });
+
+Meteor.startup(function () {
+    // initialize smooth scroll library
+    smoothScroll.init();
+    $('body').addClass('classic page');
+});
