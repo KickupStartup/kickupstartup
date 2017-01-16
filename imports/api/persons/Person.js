ca135628f2b5cd8a9caee93738550616ee39fe0b
Vitali Hatalski <v.hatalski@gmail.com>
2017-01-16 12:13:10 +0300

    refactor person object

--- imports/api/persons/Person.js
+++ imports/api/persons/Person.js
@@ -1,7 +1,44 @@
 import { Class } from 'meteor/jagi:astronomy';
 import Persons from './persons';
-import PersonInfo from './PersonInfo';
-import PersonInterests from './PersonInterests';
+
+const PersonInfo = Class.create({
+  name: 'PersonInfo',
+  fields: {
+    maritalStatus: ['single', 'married'],
+    children: Number,
+    birthDate: Date,
+    age: {
+      type: Number,
+      transient: true
+    }
+  },
+  events: {
+    afterInit(e) {
+      const doc = e.currentTarget;
+      var birthDate = doc.birthDate;
+      if (birthDate) {
+        var diff = Date.now() - birthDate.getTime();
+        doc.age = Math.abs((new Date(diff)).getUTCFullYear() - 1970);
+      }
+    }
+  }
+});
+
+const PersonInterests = Class.create({
+  name: 'PersonInterests',
+  fields: {
+    industries: [String],
+    sports: [String],
+    technologies: [String]
+  }
+});
+
+const PersonExperience = Class.create({
+  name: 'PersonExperience',
+  fields: {
+    projects: [String]
+  }
+});
 
 const Person = Class.create({
   name: 'Person',
