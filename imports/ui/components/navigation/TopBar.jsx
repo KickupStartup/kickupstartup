f6fc024f3b18934e15426d28d8447986fbc6ca8f
Vitali Hatalski <v.hatalski@gmail.com>
2017-01-27 17:20:00 +0300

    show avatar in navbar and hide profile when not logged in

--- imports/ui/components/navigation/TopBar.jsx
+++ imports/ui/components/navigation/TopBar.jsx
@@ -2,8 +2,25 @@ import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 import i18n from 'meteor/universe:i18n';
 const T = i18n.createComponent();
+import { Meteor } from 'meteor/meteor';
+import Avatar from 'react-avatar';
 
-export default class NavigationBar extends Component {
+class TopBar extends Component {
+  getProfileName() {
+    return this.props.profile ? this.props.profile.fullName : '';
+  }
+  renderProfileLink() {
+    if (Meteor.userId()) {
+      return (
+        <div className="col s2">
+          <ul className="fullpage_nav nav navbar-nav right">
+            <li><Link to="/profile" activeClassName="active" className="avatar-small">
+            <Avatar name={this.getProfileName()} size={50} round={true} /></Link></li>
+          </ul>
+        </div>
+      );
+    }
+  }
   render () {
     return (
       <div className="navbar navbar-fixed-top">
@@ -15,14 +32,16 @@ export default class NavigationBar extends Component {
                 <li><Link to="/people" activeClassName="active"><T>navigation.people</T></Link></li>
               </ul>
             </div>
-            <div className="col s2">
-              <ul className="fullpage_nav nav navbar-nav right">
-                <li><Link to="/profile" activeClassName="active" className="avatar-small"><img src="/img/avatar.jpg"/></Link></li>
-              </ul>
-            </div>
+            {this.renderProfileLink()}
           </div>
         </div>
       </div>
     )
   }
 }
+
+TopBar.propTypes = {
+  profile: PropTypes.object
+}
+
+export default TopBar;
