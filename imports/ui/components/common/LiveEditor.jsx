46b76ffa8cfb2c3ac3e4c339268a8e47bc624d7e
Vitali Hatalski <v.hatalski@gmail.com>
2017-02-08 10:08:22 +0300

    added medium-draft editor with it's default css styles

--- imports/ui/components/common/LiveEditor.jsx
+++ imports/ui/components/common/LiveEditor.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
+import {
+  Editor,
+  createEditorState,
+} from 'medium-draft';
 
 export default class LiveEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = (editorState) => this.setState({editorState});
+    this.state = {editorState: createEditorState()};
+    this.onChange = (editorState) => {
+      this.setState({editorState});
+      console.log(editorState.getCurrentContent());
+      this.props.onChange(convertToRaw(this.state.editorState.getCurrentContent());
+    }
   }
   render() {
     return (
-        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+      <Editor
+       sideButtons={[]}
+       editorState={this.state.editorState}
+       onChange={this.onChange}
+       placeholder={this.props.placeholder} />
     );
   }
 }
