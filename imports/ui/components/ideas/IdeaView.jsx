7db66b57532719aff2a1684340845efd88051f13
Vitali Hatalski <v.hatalski@gmail.com>
2017-02-09 11:28:18 +0300

    ability to display simple text idea fields (not in draft-js format)

--- imports/ui/components/ideas/IdeaView.jsx
+++ imports/ui/components/ideas/IdeaView.jsx
@@ -10,10 +10,20 @@ import {
 
 class IdeaView extends Component {
   render () {
-    let problemContent = createEditorState(JSON.parse(this.props.idea.problem)).getCurrentContent();
-    let problem = stateToHTML(problemContent);
-    let storyContent = createEditorState(JSON.parse(this.props.idea.story)).getCurrentContent();
-    let story = stateToHTML(storyContent);
+    let problem = this.props.idea.problem;
+    let story = this.props.idea.story;
+    try {
+      let parsedProblem = JSON.parse(this.props.idea.problem);
+      let parsedStory = JSON.parse(this.props.idea.story);
+      let problemContent = createEditorState(parsedProblem).getCurrentContent();
+      let storyContent = createEditorState(parsedStory).getCurrentContent();
+      problem = stateToHTML(problemContent);
+      story = stateToHTML(storyContent);
+    } catch (e) {
+      console.log("is JSON syntax error? ", e instanceof SyntaxError); // true
+      console.log("idea's problem or story not in a correct draft-js JSON format error: ", e);
+    }
+
     return (
       <div className="white row-border clearfix">
         <div className="modal-header">
