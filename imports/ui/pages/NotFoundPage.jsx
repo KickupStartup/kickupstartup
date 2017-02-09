2deb01be54cdf45c4514d061f2c2cbe993b96bba
Alexey Anshakov <alexey.anshakov@gmail.com>
2017-02-09 15:24:54 +0100

    Idea editor CSS upd

--- imports/ui/pages/NotFoundPage.jsx
+++ imports/ui/pages/NotFoundPage.jsx
@@ -5,18 +5,20 @@ import { Meteor } from 'meteor/meteor';
 export default class NotFoundPage extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="col s12">
-          <div className="white row-border">
+      <div className="container main">
+        <div className="row">
+          <div className="col s12">
+            <div className="white row-border">
               <div className="content text-center clearfix">
-                  <h3>404</h3>
-                  <h4>Page Not Found</h4>
-                  <p>
-                      The page you are looking for doesn't exist or an other error occurred.
-                      <br/>
-                      <a href="#!" onClick={browserHistory.goBack}>Go back</a>, or head over to <Link to="/">main page</Link> to choose a new direction.
-                  </p>
+                <h3>404</h3>
+                <h4>Page Not Found</h4>
+                <p>
+                  The page you are looking for doesn't exist or an other error occurred.
+                  <br/>
+                  <a href="#!" onClick={browserHistory.goBack}>Go back</a>, or head over to <Link to="/">main page</Link> to choose a new direction.
+                </p>
               </div>
+            </div>
           </div>
         </div>
       </div>
