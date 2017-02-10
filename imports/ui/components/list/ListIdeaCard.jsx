293ba84231729411475e1fcc8629a22f400a0c79
Alexey Anshakov <alexey.anshakov@gmail.com>
2017-02-10 17:15:38 +0100

    Bookmarked class color upd

--- imports/ui/components/list/ListIdeaCard.jsx
+++ imports/ui/components/list/ListIdeaCard.jsx
@@ -43,7 +43,7 @@ export default class ListIdeaCard extends Component {
     };
     return (
     <div>
-      <a href="#!" className="bookmark right"><i className="fa fa-bookmark fa-lg" title="Bookmarked"></i></a>
+      <a href="#!" className="bookmark active right"><i className="fa fa-bookmark fa-lg" title="Bookmarked"></i></a>
       <a href="#!" className="bookmark right"><i className="fa fa-bookmark-o fa-lg" title="Bookmark"></i></a>
       <div onClick={this.gotoIdeaDetails.bind(this)} className="white row-border pointer clearfix">
         <div className="content text-center clearfix">
