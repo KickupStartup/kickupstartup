dfbdfe2142dc86159bfc3f78c85a4a6bdb46e19b
Vitali Hatalski <v.hatalski@gmail.com>
2017-02-14 23:09:11 +0300

    browser policy for analytics

--- imports/startup/server/browserPolicy.js
+++ imports/startup/server/browserPolicy.js
@@ -1,3 +1,9 @@
+// google analytics
+BrowserPolicy.content.allowScriptOrigin( 'www.google.com' );
+BrowserPolicy.content.allowScriptOrigin( '*.google-analytics.com' );
+// mixpanel
+BrowserPolicy.content.allowScriptOrigin( 'cdn.mxpnl.com' );
+
 // allow roboto font download from google
 BrowserPolicy.content.allowFontOrigin( 'fonts.googleapis.com' );
 BrowserPolicy.content.allowStyleOrigin( 'fonts.googleapis.com' );
