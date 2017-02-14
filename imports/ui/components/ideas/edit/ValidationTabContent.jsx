fcfb3450f4c36c10954fe40c2b10ed478876b9e6
Vitali Hatalski <v.hatalski@gmail.com>
2017-02-14 11:19:06 +0300

    validate an idea help text

--- imports/ui/components/ideas/edit/ValidationTabContent.jsx
+++ imports/ui/components/ideas/edit/ValidationTabContent.jsx
@@ -20,7 +20,11 @@ export default class StoryTabContent extends Component {
         <div className="alert alert-info clearfix" role="alert">
           <h4><T>ideas.tabs.validation.alert.header</T></h4>
           <p><T>ideas.tabs.validation.alert.text</T></p>
+          <p><T>ideas.tabs.validation.alert.text1</T></p>
           <p><T>ideas.tabs.validation.alert.text2</T></p>
+          <p><T>ideas.tabs.validation.alert.text3</T></p>
+          <p><T>ideas.tabs.validation.alert.text4</T></p>
+          <p><T>ideas.tabs.validation.alert.text5</T></p>
         </div>
         <IdeaAskForReview idea={this.props.idea} />
         <ListDivider border={true} />
