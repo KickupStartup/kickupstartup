f610cdc17689e4a2efa991024c215648b43d5fd3
Vitali Hatalski <v.hatalski@gmail.com>
2017-02-14 23:33:53 +0300

    updated browser policies and settings file

--- imports/startup/server/browserPolicy.js
+++ imports/startup/server/browserPolicy.js
@@ -1,13 +1,23 @@
+let protocol = "https://";
+if (process.env.NODE_ENV === 'development') {
+  protocol = "http://";
+}
 // google analytics
-BrowserPolicy.content.allowScriptOrigin( 'www.google.com' );
-BrowserPolicy.content.allowScriptOrigin( '*.google-analytics.com' );
+BrowserPolicy.content.allowScriptOrigin( protocol + 'www.google.com' );
+BrowserPolicy.content.allowImageOrigin( protocol + 'www.google.com' );
+BrowserPolicy.content.allowScriptOrigin( protocol + '*.google-analytics.com' );
+BrowserPolicy.content.allowImageOrigin( protocol + '*.google-analytics.com' );
 // mixpanel
-BrowserPolicy.content.allowScriptOrigin( 'cdn.mxpnl.com' );
+BrowserPolicy.content.allowScriptOrigin( protocol + '*.mxpnl.com' );
+BrowserPolicy.content.allowImageOrigin( protocol + '*.mxpnl.com' );
+// amplitude
+BrowserPolicy.content.allowScriptOrigin( protocol + 'd24n15hnbwhuhn.cloudfront.net' );
+BrowserPolicy.content.allowImageOrigin( protocol + 'd24n15hnbwhuhn.cloudfront.net' );
 
 // allow roboto font download from google
-BrowserPolicy.content.allowFontOrigin( 'fonts.googleapis.com' );
-BrowserPolicy.content.allowStyleOrigin( 'fonts.googleapis.com' );
+BrowserPolicy.content.allowFontOrigin( protocol + 'fonts.googleapis.com' );
+BrowserPolicy.content.allowStyleOrigin( protocol + 'fonts.googleapis.com' );
 // allow material icons download from google
-BrowserPolicy.content.allowFontOrigin( 'fonts.gstatic.com' );
+BrowserPolicy.content.allowFontOrigin( protocol + 'fonts.gstatic.com' );
 // allow all content from same origin
 BrowserPolicy.content.allowSameOriginForAll();
