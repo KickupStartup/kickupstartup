fbc731cab7f68e8f446f4eaeace977f2c3cb0341
Vitali Hatalski <v.hatalski@gmail.com>
2017-02-14 16:18:22 +0300

    set browser policy on server (allow font and style from google)

---
+++ imports/startup/server/browserPolicy.js
@@ -0,0 +1,5 @@
+// allow roboto font download from google
+BrowserPolicy.content.allowFontOrigin( 'fonts.googleapis.com' );
+BrowserPolicy.content.allowStyleOrigin( 'fonts.googleapis.com' );
+// allow material icons download from google
+BrowserPolicy.content.allowFontOrigin( 'fonts.gstatic.com' );
