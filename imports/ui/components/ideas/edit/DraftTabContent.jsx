aefc7b645936b707745786fab0ed540a2c3af914
Alexey Anshakov <alexey.anshakov@gmail.com>
2017-02-17 12:36:40 +0100

    Collaborator link has been added to tips

--- imports/ui/components/ideas/edit/DraftTabContent.jsx
+++ imports/ui/components/ideas/edit/DraftTabContent.jsx
@@ -23,12 +23,16 @@ export default class DraftTabContent extends Component {
       if(result) {}
     });
   }
+  openAddCoauthorModal() {
+    $('.modal').modal();
+    $('#addCoauthor').modal('open');
+  }
   render () {
     return (
       <div className={this.props.hidden}>
         <div className="alert alert-info clearfix" role="alert">
           <h4><T>ideas.tabs.draft.alert.header</T></h4>
-          <p><T>ideas.tabs.draft.alert.text1</T><a href="#!"><T>ideas.tabs.draft.alert.text2</T></a><T>ideas.tabs.draft.alert.text3</T></p>
+          <p><T>ideas.tabs.draft.alert.text1</T><a href="#!" onClick={this.openAddCoauthorModal}><T>ideas.tabs.draft.alert.text2</T></a><T>ideas.tabs.draft.alert.text3</T></p>
         </div>
         {/* <IdeaInviteCollaborator idea={this.props.idea} />
         <ListDivider border={true} /> */}
