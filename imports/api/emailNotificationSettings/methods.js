dbe039c2edf7060c92f24578a9bd6951d29d4e34
Vitali Hatalski <v.hatalski@gmail.com>
2017-02-23 13:49:05 +0300

    server methods to update email notification settings

---
+++ imports/api/emailNotificationSettings/methods.js
@@ -0,0 +1,64 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import EmailNotification from './EmailNotification';
+
+const getValidatedEmailSettings = function(userId) {
+  if (!userId) {
+    throw new Meteor.Error('email.settings.update.unauthorized',
+      'Cannot perform any action with an idea if unauthorized.');
+  } else {
+    return EmailNotification.findOne({ userId: this.userId }) || new EmailNotification({ userId: this.userId });
+  }
+}
+
+Meteor.methods({
+  'email.settings.update.ideaCreated':function(toSend){
+     check(toSend, Boolean);
+     const settings = getValidatedEmailSettings(this.userId);
+     settings.ideaCreated = toSend;
+     settings.save();
+     return settings;
+  },
+  'email.settings.update.ideaCommented':function(toSend){
+     check(toSend, Boolean);
+     const settings = getValidatedEmailSettings(this.userId);
+     settings.ideaCommented = toSend;
+     settings.save();
+     return settings;
+  },
+  'email.settings.update.iAmMentioned':function(toSend){
+     check(toSend, Boolean);
+     const settings = getValidatedEmailSettings(this.userId);
+     settings.iAmMentioned = toSend;
+     settings.save();
+     return settings;
+  },
+  'email.settings.update.coauthorInvited':function(toSend){
+     check(toSend, Boolean);
+     const settings = getValidatedEmailSettings(this.userId);
+     settings.coauthorInvited = toSend;
+     settings.save();
+     return settings;
+  },
+  'email.settings.update.coauthorReplied':function(toSend){
+     check(toSend, Boolean);
+     const settings = getValidatedEmailSettings(this.userId);
+     settings.coauthorReplied = toSend;
+     settings.save();
+     return settings;
+  },
+  'email.settings.update.feedbackReceived':function(toSend){
+     check(toSend, Boolean);
+     const settings = getValidatedEmailSettings(this.userId);
+     settings.feedbackReceived = toSend;
+     settings.save();
+     return settings;
+  },
+  'email.settings.update.feedbackRequested':function(toSend){
+     check(toSend, Boolean);
+     const settings = getValidatedEmailSettings(this.userId);
+     settings.feedbackRequested = toSend;
+     settings.save();
+     return settings;
+  },
+});
